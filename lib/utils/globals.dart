4a4fa5a277d64c0c07da0ccda68067e6dec4946f
thtowers <83645549+thtowers@users.noreply.github.com>
2022-05-23 10:29:09 -0300

    primeiro commit
    update function button

---
+++ lib/utils/globals.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Globals {
+  static GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+}
+
+class Globals2 {
+  static GlobalKey<ScaffoldState> item2 = GlobalKey<ScaffoldState>();
+}
+
+final keySection = GlobalKey();
+final keyService = GlobalKey();
+final keyPortfolio = GlobalKey();
+
+Future scroll() async {
+  final context = keySection.currentContext;
+  await Scrollable.ensureVisible(
+    context,
+    duration: const Duration(seconds: 1),
+    alignment: 0.3,
+  );
+}
+
+Future scrollService() async {
+  final context = keyService.currentContext;
+  await Scrollable.ensureVisible(
+    context,
+    duration: const Duration(seconds: 1),
+    alignment: 0.3,
+  );
+}
+
+Future scrollPortfolio() async {
+  final context = keyPortfolio.currentContext;
+  await Scrollable.ensureVisible(
+    context,
+    duration: const Duration(seconds: 1),
+    alignment: 0.3,
+  );
+}
+
+final Uri _url = Uri.parse(
+    'https://api.whatsapp.com/send?phone=5521965524224&text=Ol%C3%A1!%20Gostaria%20de%20saber...%20');
+
+Future<void> openUrl() async {
+  if (!await launchUrl(_url)) throw 'Could not launch $_url';
+}
---
+++ lib/utils/scroll_buton.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ScrollButon extends StatelessWidget {
+  final itemKey = GlobalKey();
+
+  //Futute ScrollToItem() async {
+  //  await Scrollable.ensureVisible(context);
+  //}
+
+  ScrollButon({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
